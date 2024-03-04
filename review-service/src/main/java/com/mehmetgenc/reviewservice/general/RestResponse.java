710279e5bfe0e686a1ad48bd449770a71b426d4e
mehmetgencv <mehmetgencv@gmail.com>
2024-03-04 22:16:04 +0300

    add RestResponse

---
+++ review-service/src/main/java/com/mehmetgenc/reviewservice/general/RestResponse.java
@@ -0,0 +1,31 @@
+package com.mehmetgenc.reviewservice.general;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class RestResponse<T>{
+    private T data;
+    private LocalDateTime responseDate;
+    private boolean isSuccess;
+    private String message;
+
+    public RestResponse(T data, boolean isSuccess) {
+        this.data = data;
+        this.responseDate = LocalDateTime.now();
+        this.isSuccess = isSuccess;
+    }
+
+    public RestResponse() {
+    }
+
+    public static <T>RestResponse<T> of(T data){
+        return new RestResponse<>(data, true);
+
+    }public static <T>RestResponse<T> error(T data){
+        return new RestResponse<>(data, false);
+    }
+}
