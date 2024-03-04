18c357405ef5e2bd8172e496a4a7861174a0bedf
mehmetgencv <mehmetgencv@gmail.com>
2024-03-04 22:15:04 +0300

    add GlobalExceptionHandler

---
+++ review-service/src/main/java/com/mehmetgenc/reviewservice/advice/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.mehmetgenc.reviewservice.advice;
+
+import com.mehmetgenc.reviewservice.exception.ReviewNotFoundException;
+import com.mehmetgenc.reviewservice.exception.ReviewServiceException;
+import com.mehmetgenc.reviewservice.exception.UserNotFoundException;
+import com.mehmetgenc.reviewservice.general.GeneralErrorMessages;
+import com.mehmetgenc.reviewservice.general.RestResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleUserReviewNotFoundException(ReviewNotFoundException exception, WebRequest request){
+        String message = exception.getMessage();
+        String description = request.getDescription(false);
+        GeneralErrorMessages generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
+        RestResponse restResponse = RestResponse.error(generalErrorMessages);
+        return new ResponseEntity<>(restResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception, WebRequest request){
+        String message = exception.getMessage();
+        String description = request.getDescription(false);
+        GeneralErrorMessages generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
+        RestResponse restResponse = RestResponse.error(generalErrorMessages);
+        return new ResponseEntity<>(restResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request){
+        String message = e.getMessage();
+        String description = request.getDescription(false);
+
+        GeneralErrorMessages generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
+        RestResponse restResponse = RestResponse.error(generalErrorMessages);
+        return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleRunTimeExceptions(RuntimeException e, WebRequest request){
+        String message = e.getMessage();
+        String description = request.getDescription(false);
+
+        GeneralErrorMessages generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
+        RestResponse restResponse = RestResponse.error(generalErrorMessages);
+        return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleReviewServiceExceptions(ReviewServiceException e, WebRequest request){
+        String message = e.getMessage();
+        String description = request.getDescription(false);
+
+        GeneralErrorMessages generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
+        RestResponse restResponse = RestResponse.error(generalErrorMessages);
+        return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
