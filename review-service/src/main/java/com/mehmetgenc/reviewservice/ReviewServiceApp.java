d20152b3f219c2e2c19d33b70aa872bebe21c82b
mehmetgencv <mehmetgencv@gmail.com>
2024-03-05 21:27:07 +0300

    add application.properties

--- review-service/src/main/java/com/mehmetgenc/reviewservice/ReviewServiceApp.java
+++ review-service/src/main/java/com/mehmetgenc/reviewservice/ReviewServiceApp.java
@@ -1,7 +1,11 @@
 package com.mehmetgenc.reviewservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ReviewServiceApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ReviewServiceApp.class, args);
     }
 }
