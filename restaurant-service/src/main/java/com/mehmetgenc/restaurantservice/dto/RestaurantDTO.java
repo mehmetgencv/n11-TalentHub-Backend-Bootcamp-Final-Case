ac5ece5880ac1c4b0febcd9a6aa12e75a737930f
mehmetgencv <mehmetgencv@gmail.com>
2024-03-06 20:15:04 +0300

    update RestaurantDTO

--- restaurant-service/src/main/java/com/mehmetgenc/restaurantservice/dto/RestaurantDTO.java
+++ restaurant-service/src/main/java/com/mehmetgenc/restaurantservice/dto/RestaurantDTO.java
@@ -3,6 +3,7 @@ package com.mehmetgenc.restaurantservice.dto;
 public record RestaurantDTO(
         Long id,
         String name,
+        Double rate,
         String address,
         String phone,
         String email
