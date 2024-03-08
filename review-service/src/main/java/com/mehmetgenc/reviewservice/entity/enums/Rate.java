872cd79d1f0d0fba5a84dd8c2ef65e6efd52bad4
mehmetgencv <mehmetgencv@gmail.com>
2024-03-08 20:53:48 +0300

    update enums

--- review-service/src/main/java/com/mehmetgenc/reviewservice/entity/enums/Rate.java
+++ review-service/src/main/java/com/mehmetgenc/reviewservice/entity/enums/Rate.java
@@ -7,9 +7,15 @@ import lombok.Setter;
 @RequiredArgsConstructor
 @Getter
 public enum Rate {
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE
+    ONE(1.0),
+    TWO(2.0),
+    THREE(3.0),
+    FOUR(4.0),
+    FIVE(5.0);
+
+    private final double value;
+
+    public double getValue() {
+        return value;
+    }
 }
