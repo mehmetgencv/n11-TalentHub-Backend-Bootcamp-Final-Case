f19e1b4c00d8758119afd3b2e7b47c2ff2538ee0
mehmetgencv <mehmetgencv@gmail.com>
2024-03-09 01:29:40 +0300

    add getAllRequest

--- review-service/src/main/java/com/mehmetgenc/reviewservice/controller/RestaurantController.java
+++ review-service/src/main/java/com/mehmetgenc/reviewservice/controller/RestaurantController.java
@@ -3,6 +3,7 @@ package com.mehmetgenc.reviewservice.controller;
 import com.mehmetgenc.reviewservice.client.RestaurantServiceClient;
 import com.mehmetgenc.reviewservice.controller.contract.RestaurantControllerContract;
 import com.mehmetgenc.reviewservice.dto.RestaurantInfoDTO;
+import com.mehmetgenc.reviewservice.dto.RestaurantRecommendInfoDTO;
 import com.mehmetgenc.reviewservice.general.RestResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/restaurants")
@@ -27,6 +30,12 @@ public class RestaurantController {
     @PatchMapping("/updateRate")
     public RestResponse<RestaurantInfoDTO> updateRate(Long restaurantId, Double rate) {
         return restaurantControllerContract.updateRate(restaurantId, rate);
+    }
+
+    // get ALL restaurants
+    @GetMapping
+    public RestResponse<List<RestaurantRecommendInfoDTO>> getAllRestaurants() {
+        return restaurantControllerContract.getAllRestaurants();
     }
 
 }
