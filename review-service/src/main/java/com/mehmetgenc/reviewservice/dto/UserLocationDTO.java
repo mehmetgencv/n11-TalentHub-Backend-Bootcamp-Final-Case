fefa86e5b1aac7c97626c91b88e9477044213c7c
mehmetgencv <mehmetgencv@gmail.com>
2024-03-09 01:30:03 +0300

    add UserLocationDTO

---
+++ review-service/src/main/java/com/mehmetgenc/reviewservice/dto/UserLocationDTO.java
@@ -0,0 +1,6 @@
+package com.mehmetgenc.reviewservice.dto;
+
+public record UserLocationDTO(Long id,
+                              Double latitude,
+                              Double longitude) {
+}
