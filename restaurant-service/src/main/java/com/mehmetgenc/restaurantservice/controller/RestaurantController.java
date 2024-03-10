c8389a4af02cac94ad36d4c224737ab1243ae726
mehmetgencv <mehmetgencv@gmail.com>
2024-03-10 23:00:54 +0300

    add swagger documentation for restaurant-service

--- restaurant-service/src/main/java/com/mehmetgenc/restaurantservice/controller/RestaurantController.java
+++ restaurant-service/src/main/java/com/mehmetgenc/restaurantservice/controller/RestaurantController.java
@@ -4,6 +4,8 @@ import com.mehmetgenc.restaurantservice.controller.contract.RestaurantController
 import com.mehmetgenc.restaurantservice.dto.RestaurantDTO;
 import com.mehmetgenc.restaurantservice.dto.RestaurantSaveRequest;
 import com.mehmetgenc.restaurantservice.general.RestResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/restaurants")
 @Validated
+@Tag(name = "Restaurant Controller", description = "Restaurant Management")
 public class RestaurantController {
     private final RestaurantControllerContract restaurantControllerContract;
 
@@ -22,36 +25,42 @@ public class RestaurantController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new restaurant", description = "Creates a new restaurant with the provided details")
     public ResponseEntity<RestResponse<RestaurantDTO>> saveRestaurant(@RequestBody @Valid RestaurantSaveRequest restaurantSaveRequest){
         RestaurantDTO restaurantDto = restaurantControllerContract.save(restaurantSaveRequest);
         return ResponseEntity.ok(RestResponse.of(restaurantDto));
     }
 
     @GetMapping
+    @Operation(summary = "Get all restaurants", description = "Retrieves all active restaurants")
     public ResponseEntity<RestResponse<Iterable<RestaurantDTO>>> getAllRestaurants(){
         Iterable<RestaurantDTO> restaurantDtoList = restaurantControllerContract.getAll();
         return ResponseEntity.ok(RestResponse.of(restaurantDtoList));
     }
 
     @GetMapping("/{restaurantId}")
+    @Operation(summary = "Get Restaurant by ID", description = "Returns a single restaurant based on the provided ID")
     public ResponseEntity<RestResponse<RestaurantDTO>> getRestaurantById(@PathVariable String restaurantId){
         RestaurantDTO restaurantDto = restaurantControllerContract.getById(restaurantId);
         return ResponseEntity.ok(RestResponse.of(restaurantDto));
     }
 
     @DeleteMapping("/{restaurantId}")
+    @Operation(summary = "Delete Restaurant by ID", description = "Deletes a single restaurant based on the provided ID")
     public ResponseEntity<RestResponse<String>> deleteRestaurantById(@PathVariable String restaurantId){
         restaurantControllerContract.delete(restaurantId);
         return ResponseEntity.ok(RestResponse.of("Restaurant deleted"));
     }
 
     @PostMapping("/updateRate/{restaurantId}")
+    @Operation(summary = "Update rate of the restaurant By Restaurant ID", description = "Updates a single restaurant rate based on the provided ID")
     public ResponseEntity<RestResponse<RestaurantDTO>> updateRateRestaurant(@PathVariable String restaurantId, @RequestParam Double rate){
         RestaurantDTO restaurantDto = restaurantControllerContract.updateRate(restaurantId, rate);
         return ResponseEntity.ok(RestResponse.of(restaurantDto));
     }
 
     @PostMapping("/saveBatchRestaurants")
+    @Operation(summary = "Create new restaurants", description = "Creates new restaurants with the provided details")
     public ResponseEntity<RestResponse<List<RestaurantDTO>>> saveBatchRestaurants(@RequestBody List<RestaurantSaveRequest> restaurantSaveRequestList){
         List<RestaurantDTO> restaurantDtoList = restaurantControllerContract.saveBatch(restaurantSaveRequestList);
         return ResponseEntity.ok(RestResponse.of(restaurantDtoList));
