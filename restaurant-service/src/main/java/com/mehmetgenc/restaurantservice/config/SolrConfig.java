75f18274231075c350cab67c9de61d5202a661d4
mehmetgencv <mehmetgencv@gmail.com>
2024-03-15 02:18:54 +0300

    update paths

--- restaurant-service/src/main/java/com/mehmetgenc/restaurantservice/config/SolrConfig.java
+++ restaurant-service/src/main/java/com/mehmetgenc/restaurantservice/config/SolrConfig.java
@@ -2,6 +2,7 @@ package com.mehmetgenc.restaurantservice.config;
 
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +18,12 @@ import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 @ComponentScan
 public class SolrConfig {
 
+  @Value("${solr.service.url}")
+    private String solrServiceUrl;
+
   @Bean
   public SolrClient solrClient(){
-    return new HttpSolrClient.Builder("http://172.17.0.1:8983/solr").build();
+    return new HttpSolrClient.Builder(solrServiceUrl).build();
   }
 
   @Bean
--- review-service/src/main/java/com/mehmetgenc/reviewservice/client/RestaurantServiceClient.java
+++ review-service/src/main/java/com/mehmetgenc/reviewservice/client/RestaurantServiceClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@FeignClient(value = "restaurantService", url = "http://localhost:8081/api/v1/restaurants")
+@FeignClient(value = "restaurantService", url = "${restaurant.service.url}")
 public interface RestaurantServiceClient {
 
     @GetMapping("/{userId}")
